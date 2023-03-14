46ad432dd4ef7167ad734c1b1af895ab77cb03e3
Arthur Cluet <a.cluet@live.fr>
2023-03-14 22:33:30 +0100

    Scraper no longer saves HTML files

--- scraper.sh
+++ scraper.sh
@@ -2,15 +2,9 @@
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-# Downloading differents values
-curl -s https://www.coindesk.com/price/cardano/ > "$parent_path/data/coindesk_ada.html"
-curl -s --compressed https://coinmarketcap.com/currencies/ethereum/ > "$parent_path/data/coinmarketcap_eth.html"
-curl -s https://coinpaprika.com/coin/btc-bitcoin/ > "$parent_path/data/coinpaprika_btc.html"
-
-# Getting value from HTML
-cd_ada=$(cat $parent_path/data/coindesk_ada.html |  grep -o -P '(?<=<span class=\"currency-pricestyles__Price-sc-1rux8hj-0 jxzQXk\">).*(?=</span></div><div class=\"Box-sc-1hpkeeg-0 bWliNl)')
-cmc_eth=$(cat $parent_path/data/coinmarketcap_eth.html |  grep -o -P '(?<=<div class=\"priceValue \"><span>).*(?=</span></div><span style=)' | head -1)
-cp_btc=$(cat $parent_path/data/coinpaprika_btc.html |  grep -A 1 '<span id=\"coinPrice\"' | tail -1 )
+cd_ada=$(curl -s https://www.coindesk.com/price/cardano/ |  grep -o -P '(?<=<span class=\"currency-pricestyles__Price-sc-1rux8hj-0 jxzQXk\">).*(?=</span></div><div class=\"Box-sc-1hpkeeg-0 bWliNl)')
+cmc_eth=$(curl -s --compressed https://coinmarketcap.com/currencies/ethereum/ |  grep -o -P '(?<=<div class=\"priceValue \"><span>).*(?=</span></div><span style=)' | head -1)
+cp_btc=$(curl -s https://coinpaprika.com/coin/btc-bitcoin/ |  grep -A 1 '<span id=\"coinPrice\"' | tail -1 )
 
 # Saving value into a file
 echo "$(date +'%Y-%m-%d %H:%M:%S');$cd_ada" >> $parent_path/data/coindesk_ada.txt
@@ -29,14 +23,5 @@ then
 	echo "ABOUT"
 	echo -e "  This script downloads a page containing current bitcoin price and saves\n  the new price in a file on a new line.\n"
     echo "PARAMETERS"
-	echo "  -h, --help	Display the manual"
-	echo "  --verbose, -v"	Verbose
-fi
-
-# Verbose option
-if [[ "$*" == *"--verbose"* ]] || [[ "$*" == *"-v"* ]]
-then
-    echo -e "- Web page downloaded and saved in data/coingecko_btc.html"
-	echo -e "- Bitcoin price saved in data/coingecko_btc.txt"
-	echo -e "Current bitcoin price is: ${YELLOW}\$$bitcoin${NC}"
+	echo "  No parameters yet"
 fi
