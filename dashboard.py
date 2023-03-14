e7878045683ac31710e1142d9f4b1a10873f1ad8
Arthur Cluet <a.cluet@live.fr>
2023-03-14 22:10:47 +0100

    Main update

--- dashboard.py
+++ dashboard.py
@@ -2,44 +2,106 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input, Output
+import numpy as np
 
+# Function that reads prices from the files
+# It removes any unwanted characters from strings such as "$24,393" and parses it as a float
+# Then if an error occured and a value can't be used, it just drops the line containing NA
+
+def mapFunction(x):
+    try:
+        return float(x.replace(' ', '').replace('$', '').replace(',', '')) if isinstance(x, str) else x 
+    except:
+        return np.nan
+files = {
+    "ADA": "./data/coindesk_ada.txt",
+    "BTC": "./data/coinpaprika_btc.txt",
+    "ETH": "./data/coinmarketcap_eth.txt"
+}
+def readPrices(coin):
+    df = pd.read_csv(files[coin], sep=";", names=["date", coin])
+    df[coin] = df[coin].map(mapFunction)
+    df = df.dropna()
+    return df
+
+# Dashboard Layout
 app = Dash(__name__)
-
-
-def readData():
-    f = open("./data/coinmarketcap_eth.txt","r")
-    lines = f.readlines()
-    def getDates(s):
-        return s.split('|')[0].replace(',', '').replace('\n', '')
-    def getVal(s):
-        return float(s.split('|')[1].replace(',', '').replace('\n', '').replace('$', ''))
-    dataDates = list(map(getDates, lines))
-    dataValues = list(map(getVal, lines))
-    return pd.DataFrame({'date':dataDates, 'ETH':dataValues})
+app.title = "Crypto Prices"
 
 app.layout = html.Div(className="app", children=[
-    html.H1(children='Ethereum Price'),
+    html.H1(children='Cryptocurrency Prices'),
 
     html.Div(className="description", children='''
-        Dash: A web application framework for your data.
+        A dashboard following the evolution of the price of some cryptocurrencies
     '''),
+
+html.Div(children='Cryptocurrency:', className="label"),
+
+    dcc.Dropdown(options=[
+       {'label': 'Bitcoin (BTC)', 'value': 'BTC'},
+       {'label': 'Ethereum (ETC)', 'value': 'ETH'},
+       {'label': 'Cardano (ADA)', 'value': 'ADA'},
+   ],
+   value='BTC', id='coin-dropdown'),
+
+
+
+    html.Div(children='SMA periods:', className="label"),
+    html.Div(children=[
+        html.Span('Increase SMA period below to display bollinger bands on the graph. Choose 0 to hide them.'),
+        html.Br(),
+        html.Span('Note: Lines can be hidden by clicking on the legend.')
+    ], className='help'),
+
+   dcc.Slider(min=0, max=50, step=1, value=0, className="slider", id="sma-slider", marks={0: '0', 50:'50'}),
+
+    
 
     dcc.Graph(
         id='price-graph'
     ),
     dcc.Interval(
             id='interval-component',
-            interval=30*1000, # every 30 seconds
+            interval=90*1000, # every 90 seconds
             n_intervals=0
         )
 ])
 
+# Callback, updates the shown figure depending on two inputs
 @app.callback(Output('price-graph', 'figure'),
-              Input('interval-component', 'n_intervals'))
-def update_graph(n):
-    bitcoinDf = readData()
-    fig = px.line(bitcoinDf, x='date', y="ETH")
+              Input('interval-component', 'n_intervals'),
+              Input('coin-dropdown', 'value'),
+              Input('sma-slider', 'value'))
+def update_graph(n, value, sma):
+    df = readPrices(value)
+    #fig = px.line(df, x='date', y=value)
+    y = [value]
+
+    fig = px.line(df, x='date', y=y)
+
+    if sma > 0:
+        df['SMA'] = df[value].rolling(sma).mean()
+        df.dropna(inplace=True)
+        fig.add_scatter(x=df['date'], y=df['SMA'], mode='lines', name="SMA")
+        if sma > 1:
+            delta = 1
+            df['STD'] = df[value].rolling(sma).std()
+            df['bb_low'] = df['SMA'] - delta * df['STD']
+            df['bb_up'] = df['SMA'] + delta * df['STD']
+            fig.add_scatter(x=df['date'], y=df['bb_up'], mode='lines', name="Upper band")
+            fig.add_scatter(x=df['date'], y=df['bb_low'], mode='lines', name="Lower band")
+
+    
+
+    fig.update_layout(legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ))
     return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=3000, host= '0.0.0.0')
--- scraper.sh
+++ scraper.sh
@@ -3,19 +3,19 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 # Downloading differents values
-curl -s https://www.coingecko.com/en/coins/bitcoin > "$parent_path/data/coingecko_btc.html"
+curl -s https://www.coindesk.com/price/cardano/ > "$parent_path/data/coindesk_ada.html"
 curl -s --compressed https://coinmarketcap.com/currencies/ethereum/ > "$parent_path/data/coinmarketcap_eth.html"
 curl -s https://coinpaprika.com/coin/btc-bitcoin/ > "$parent_path/data/coinpaprika_btc.html"
 
 # Getting value from HTML
-bitcoin=$(cat $parent_path/data/coingecko_btc.html |  grep -o -P '(?<=\$).*(?=</span></span>)' | head -1)
+cd_ada=$(cat $parent_path/data/coindesk_ada.html |  grep -o -P '(?<=<span class=\"currency-pricestyles__Price-sc-1rux8hj-0 jxzQXk\">).*(?=</span></div><div class=\"Box-sc-1hpkeeg-0 bWliNl)')
 cmc_eth=$(cat $parent_path/data/coinmarketcap_eth.html |  grep -o -P '(?<=<div class=\"priceValue \"><span>).*(?=</span></div><span style=)' | head -1)
 cp_btc=$(cat $parent_path/data/coinpaprika_btc.html |  grep -A 1 '<span id=\"coinPrice\"' | tail -1 )
 
 # Saving value into a file
-echo "$(date +'%Y-%m-%d %H:%M:%S')|$bitcoin" >> $parent_path/data/coingecko_btc.txt
-echo "$(date +'%Y-%m-%d %H:%M:%S')|$cmc_eth" >> $parent_path/data/coinmarketcap_eth.txt
-echo "$(date +'%Y-%m-%d %H:%M:%S')|$cp_btc" >> $parent_path/data/coinpaprika_btc.txt
+echo "$(date +'%Y-%m-%d %H:%M:%S');$cd_ada" >> $parent_path/data/coindesk_ada.txt
+echo "$(date +'%Y-%m-%d %H:%M:%S');$cmc_eth" >> $parent_path/data/coinmarketcap_eth.txt
+echo "$(date +'%Y-%m-%d %H:%M:%S');$cp_btc" >> $parent_path/data/coinpaprika_btc.txt
 
 # Adding options to the script :
 
