36cd86f57dccb4c50ea3765b6c5bf8ecc94ce3ca
Arthur Cluet <a.cluet@live.fr>
2023-03-24 00:18:12 +0100

    scraper, gitignore update

--- scraper.sh
+++ scraper.sh
@@ -4,9 +4,19 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 # Getting values from websites
+
+# Curl classique sans afficher le chargement avec l'option silent
+# J'utilise ensuite "grep -o -P '(?<=DEBUT).*(?=FIN)'" pour chercher la valeur entre deux mots
 cd_ada=$(curl -s https://www.coindesk.com/price/cardano/ |  grep -o -P '(?<=<span class=\"currency-pricestyles__Price-sc-1rux8hj-0 jxzQXk\">).*(?=</span></div><div class=\"Box-sc-1hpkeeg-0 bWliNl)')
-cmc_eth=$(curl -s --compressed https://coinmarketcap.com/currencies/ethereum/ |  grep -o -P '(?<=<div class=\"priceValue \"><span>).*(?=</span></div><span style=)' | head -1)
-cp_btc=$(curl -s https://coinpaprika.com/coin/btc-bitcoin/ |  grep -A 1 '<span id=\"coinPrice\"' | tail -1 )
+# J'ai utilisé le flag --compressed puisque CoinMarketCap utilise la compression pour rendre le chargement du site plus rapide
+# J'utilise ensuite "grep -o -P '(?<=DEBUT).*(?=FIN)'" pour chercher la valeur entre deux mots
+# J'enlève la virgule et le $ avec sed
+cmc_eth=$(curl -s --compressed https://coinmarketcap.com/currencies/ethereum/ |  grep -o -P '(?<=<div class=\"priceValue \"><span>).*(?=</span></div><span style=)' | sed -e 's/,//g' -e 's/\$//g')
+# Sur ce site, le prix est sur une ligne juste après la ligne contenant la balise span.CoinPrice
+# J'ai donc choisi de chercher la balise et afficher la ligne suivant le résultat
+# Et enfin garder uniquement la dernière ligne entre les deux avec tail
+# Enfin, avec sed je supprime le signe $ et l'espace
+cp_btc=$(curl -s https://coinpaprika.com/coin/btc-bitcoin/ |  grep -A 1 '<span id=\"coinPrice\"' | tail -1 | sed -e 's/ //g' -e 's/\$//g')
 
 # Saving values into files
 echo "$(date +'%Y-%m-%d %H:%M:%S');$cd_ada" >> $parent_path/data/coindesk_ada.txt
